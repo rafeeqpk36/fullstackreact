04affce6f5a013d02968490e3348a51c575b2154
rafeeqpk <rafeeqpk36@gmail.com>
2022-12-01 21:10:59 +0530

    react combing reducers

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,7 +1,112 @@
-import React from "react"
-import{ Card,CardImg,CardBody,CardTitle,CardText,Breadcrumb,BreadcrumbItem} from "reactstrap"
+import React,{Component} from "react"
+import{ Card,CardImg,CardBody,CardTitle,CardText,Breadcrumb,BreadcrumbItem,Button,Modal,ModalHeader,ModalBody,Label,Col,Row} from "reactstrap"
 import {Link} from "react-router-dom"
+import {Control,LocalForm,Errors} from "react-redux-form"
+const required=(val)=>val&&val.length;
+const maxLength=(len)=>(val)=>!(val)||(val.length<=len);
+const minLength=(len)=>(val)=>(val)&&(val.length>=len);
 
+ class CommentForm extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            isModalOpen:false
+        }
+    
+        this.toggleModal=this.toggleModal.bind(this)
+        this.handleSubmit=this.handleSubmit.bind(this)
+    }
+    toggleModal(){
+        this.setState({
+            isModalOpen:!this.state.isModalOpen
+        })
+    }
+    handleSubmit(values){
+        console.log("current state is : " + JSON.stringify(values));
+        alert("current state is : " + JSON.stringify(values))
+        
+    };
+    
+    render(){
+        return(<React.Fragment><Button outline  onClick={this.toggleModal}>
+            <span className="fa fa-comments fa-lg"></span> Submit Comment</Button>
+            <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+        <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+        <ModalBody>
+        <LocalForm onSubmit={(values)=>this.handleSubmit(values)}>
+        <Row className="form-group"> 
+                            <Label htmlFor="rating" md={12}>Rating</Label>                               
+                                <Col md={12}>
+                                    <Control.select model=".rating" name="rating"
+                                    className='form-control'
+                                    id="rating" 
+                                    validators={{required}}
+                                   >    
+                                   <option>Please Select</option>
+                                   <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </Control.select>
+                                    <Errors className='text-danger'
+    model=".rating"
+    show="touched"
+    messages={{
+        required:'Required'
+    }}/>
+                                </Col>
+                            </Row>
+                        <Row className="form-group">
+<Label htmlFor="yourname" md={12}>Your Name</Label>
+<Col md={12}>
+    <Control.text model=".yourname" id="yourname" name="yourname" 
+    className="form-control" placeholder="Your Name"
+    validators={{required,minLength:minLength(3),maxLength:maxLength(15)}}
+    />
+    <Errors className='text-danger'
+    model=".yourname"
+    show="touched"
+    messages={{
+        required:'Required',
+        maxLength:'Must be 15 characters or less',
+        minLength:'Must be greater than 2 characters'
+    }}/>
+
+</Col>
+                        </Row>                    
+                            
+                            <Row className="form-group">
+                                <Label htmlFor="comment" md={12}>Comment</Label>
+                                <Col md={12}>
+                                    <Control.textarea model=".comment" id="comment" 
+                                    name="comment" rows="6"
+                                    className='form-control'
+                                    validators={{required}} 
+                                                                     
+                                        />
+                                        <Errors className='text-danger'
+    model=".comment"
+    show="touched"
+    messages={{
+        required:'Required'
+    }}/>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col md={6}>
+                                    <Button type="submit" color="primary">
+                                        Submit
+                                    </Button>
+                                </Col>
+                            </Row>
+                    </LocalForm>           
+        </ModalBody>
+    </Modal>
+    </React.Fragment>)
+
+    }
+ }
 function RenderDish({dish}){
         if(dish!=null){return(
             <div className=" row col-12  col-md-5  m-1">
@@ -23,6 +128,8 @@ function RenderDish({dish}){
   function  RenderComments({commnts}){
                     const comm = commnts.map((commnt) => {
                 return (
+                    
+                    
             <li key={commnt.id} >
             
                 <p>{commnt.comment}</p>
@@ -32,7 +139,12 @@ function RenderDish({dish}){
                         month: 'long',
                         day: '2-digit'
                     }).format(new Date(commnt.date))}</p>
-                </li>)
+                    
+                </li>
+                
+            
+                
+                )
             
         })
          if(commnts!=null){return(<div className="col-12 col-md-5 m-1">
@@ -40,6 +152,7 @@ function RenderDish({dish}){
             <ul className="ist-unstyled">
             {comm}
             </ul>
+            <CommentForm/>
             </div>)}
             else{
                 return(<div></div>)
@@ -68,8 +181,10 @@ function RenderDish({dish}){
     </div>
           <div className='row'>
             <RenderDish dish={props.dish} />
-            <RenderComments commnts={props.comments}/>                       
+            <RenderComments commnts={props.comments} />
+                               
             </div>
+            
             </div>
         
       )
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,15 @@
-import {createStore} from "redux"
-import { Reducer, initialState } from "./reducer"
+import {createStore,combineReducers} from "redux"
+import { Dishes } from "./dishes"
+import { Comments } from "./comments"
+import { Leaders } from "./leaders"
+import { Promotions } from "./promotions"
 
 export const configureStore=()=>{
-    const store=createStore(Reducer, initialState)
+    const store=createStore(combineReducers({
+        dishes:Dishes,
+        comments:Comments,
+        leaders:Leaders,
+        promotions:Promotions
+    }))
     return store
 }
